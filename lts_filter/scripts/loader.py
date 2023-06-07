6fe766bbadcbeadda6119d92a3b40f3d5adfc536
ibrahim <ibrahim.hroub7@gmail.com>
2023-06-07 07:14:09 +0000

    fix some bugs in the loader script

--- lts_filter/scripts/loader.py
+++ lts_filter/scripts/loader.py
@@ -1,57 +1,31 @@
-import os
+#!/usr/bin/env python3
+
 import numpy as np
-from tqdm import tqdm
 from typing import List, Tuple
 from torch.utils.data import Dataset
 
-class ScansDataLoader(Dataset):
-    def __init__(self, dir_path: str, scans: List[str], lidar: str = 'vlp-16') -> None:
+class Loader(Dataset):
+    def __init__(self, data, lidar: str = 'vlp-16') -> None:
         super().__init__()
 
-        assert lidar in {'vlp-16', 'hdl-32', 'hdl-64'}, 'lidar type should be \'vlp-16\' or \'hdl-32\''
-
-        self.frames = []
-        self.scan_id = []
+        assert lidar in {'vlp-16'}, 'lidar type should be \'vlp-16\''
 
         lidar_params = {
             'vlp-16': {
                 'num_beams': 16,
                 'fov_up': 16.8,
                 'fov_down': -16.8,
-            }
+                'window_size': 128,
+            },
         }
         lidar_param = lidar_params[lidar]
 
         self.num_slices = 512
-
-        for scan in tqdm(scans, total=len(scans)):
-            data = np.loadtxt(os.path.join(dir_path, scan))
-            frame = self.lidar_to_image(data, lidar_param, self.num_slices)
-            self.frames.append(frame)
-            self.scan_id.append(scan[:-4])
-
-        scan_idxs = [idx for idx in range(len(scans))]
-        self.scan_idxs = np.array(scan_idxs)
-        print(f"Total samples {len(scan_idxs)}.")
+        self.num_windows = 1
+        self.frame = self.lidar_to_image(data, lidar_param, self.num_slices)
 
 
     def lidar_to_image(self, data: np.ndarray, lidar_param: dict, num_slices: int) -> np.ndarray:
-        """
-        Convert LiDAR points to an image.
-
-        Args:
-        data: A numpy array of shape (N, 4), where N is the number of points and each row
-            contains (x, y, z, s) coordinates, where 's' is the stability score.
-        lidar_param->num_beams: Number of beams in the LiDAR sensor.
-        lidar_param->fov_up: Field of view angle in degrees in the upward direction.
-        lidar_param->fov_down: Field of view angle in degrees in the downward direction.
-
-        num_slices: Number of slices (azimuth angle) in the range image.
-
-        Returns:
-        range_image: A numpy array of shape (ibrahim@10.5.37.139:/home/ibrahim/neptune/pn2-reg/data/., num_slices) representing the range image.
-        """
-
         data = np.unique(data, axis=0)
         data = data[data[:, 3] != -1]
 
@@ -67,44 +41,23 @@ class ScansDataLoader(Dataset):
         phi_idx = np.floor(phi / phi_res).astype(np.int32)
 
         num_channels = 4
-        range_image = np.zeros((lidar_param['num_beams'], num_slices, num_channels), dtype=np.float32)
+        projected_data = np.zeros((lidar_param['num_beams'], num_slices, num_channels), dtype=np.float32)
 
-        # adjust the indices to ensure they are within bounds
-        theta_idx[theta_idx >= lidar_param['num_beams']] = lidar_param['num_beams'] - 1
+        projected_data[theta_idx, phi_idx, 0] = x
+        projected_data[theta_idx, phi_idx, 1] = y
+        projected_data[theta_idx, phi_idx, 2] = z
+        projected_data[theta_idx, phi_idx, 3] = s
 
-        range_image[theta_idx, phi_idx, 0] = x
-        range_image[theta_idx, phi_idx, 1] = y
-        range_image[theta_idx, phi_idx, 2] = z
-        range_image[theta_idx, phi_idx, 3] = s
-
-        return range_image
+        return projected_data
 
 
     def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray, str]:
-        scan_idx = self.scan_idxs[idx]
-        frame = self.frames[scan_idx]
-
-        frame = frame[:, :, :].reshape(-1, frame.shape[-1])
+        frame = self.frame[:, :, :].reshape(-1, self.frame.shape[-1])
 
         points = frame[:, :3]
         labels = frame[:, 3]
 
-        return points, labels, f"{self.scan_id[scan_idx]}"
+        return points, labels
 
     def __len__(self) -> int:
-        return len(self.scan_idxs)
-
-
-if __name__ == '__main__':
-    
-    FRAMES_DIR = '/home/ibrahim/Documents/AnyDesk'
-    frames = os.listdir(FRAMES_DIR)
-
-    wdl = ScansDataLoader(FRAMES_DIR, frames)
-
-    print(len(wdl))
-
-    for i in range(0, len(wdl)):
-        data, _, id = wdl[i]
-        np.savetxt(os.path.join(FRAMES_DIR,id+'.asc'), data, fmt='%.3e')
-    
+        return self.num_windows
